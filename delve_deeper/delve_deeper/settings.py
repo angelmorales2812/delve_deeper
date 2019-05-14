f38b3d04a8188be91f9a21f9a93d4696c8ef57a1
angelmorales2812 <33816123+angelmorales2812@users.noreply.github.com>
2019-05-14 16:26:19 -0400

    Update settings.py
    update

--- delve_deeper/delve_deeper/settings.py
+++ delve_deeper/delve_deeper/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/home/r2-d2/delve_deeper/delve_deeper/static/',
-]
+STATIC_ROOT = '/home/r2-d2/delve_deeper/delve_deeper/static/'
+
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'

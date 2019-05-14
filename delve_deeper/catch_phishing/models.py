24cde37ae5f0a51c917bb511c1230301a3b7ade5
angelmorales2812 <33816123+angelmorales2812@users.noreply.github.com>
2019-05-14 12:06:19 -0400

    Update models.py
    Modelo catch_phishing

--- delve_deeper/catch_phishing/models.py
+++ delve_deeper/catch_phishing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class catch_phishing(models.Model):
+	assumed_phishing = models.URLField()
+	validate = models.BooleanField(initial=False)
+	validate_user = models.ForeignKey(user)
+
+class phishing_report():
+	url = models.URLField()
+	screenshot = models.ImageField(upload_to='screentshot/', blank=True)
+	submitted_by = models.ForeignKey(user)
+	whois = models.CharField()
+
+class entities():
+	organization = models.CharField(max_length=100)
